cccce36802c801ffe11dc74ef6cdbd6929ef93c0
artempr <artyom.prishepa@gmail.com>
2016-03-10 18:13:48 +0600

    .

--- AGIServer.go
+++ AGIServer.go
@@ -13,11 +13,13 @@ import (
 	"time"
 	"regexp"
 	"encoding/json"
+	"github.com/sdidyk/mtproto"
+	"strconv"
 )
 
 const (
-	name        = "agiserver"
-	description = "AGIServer"
+	_DAEMON_NAME    = "agiserver"
+	_DAEMON_DESC 	= "AGIServer"
 	ipaddr 		= "127.0.0.1"
 	port 		= "4573"
 )
@@ -27,15 +29,21 @@ var (
 	ALLOW []string //ALLOW NETWORKS
 	DENY []string //DENY NETWORKS
 	stdlog, errlog *log.Logger
+	TG []string
 )
 
 type Config struct {
 	Network Network
+	Tg Tg
 }
 
 type Network struct {
 	Allow []string
 	Deny []string
+}
+
+type Tg struct {
+	Rcp []string
 }
 
 type Service struct {
@@ -160,7 +168,7 @@ func BanIpFromPSTN(mm map[string]string) {
 	var BAN = make(map[string]string)
 	rex, err := regexp.Compile(`^sip:(\S+)\@(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\:(\S+)$`)
 	res := rex.FindStringSubmatch(mm["sipuri"])
-	if res {
+	if res != nil {
 		BAN["num"] = res[1]
 		BAN["ip"] = res[2]
 		BAN["port"] = res[3]
@@ -168,24 +176,76 @@ func BanIpFromPSTN(mm map[string]string) {
 
 	rex1, err := regexp.Compile(`^sip:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$`)
 	res1 := rex1.FindStringSubmatch(mm["sipuri"])
-	if res1 {
-		BAN["num"] = nil
+	if res1 != nil {
+		BAN["num"] = ""
 		BAN["ip"] = res1[1]
-		BAN["port"] = nil
+		BAN["port"] = ""
 	}
 
 	rex2, err := regexp.Compile(`^sip:(\S+)\@(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$`)
 	res2 := rex2.FindStringSubmatch(mm["sipuri"])
-	if res2 {
+	if res2 != nil {
 		BAN["num"] = res2[1]
 		BAN["ip"] = res2[2]
-		BAN["port"] = nil
+		BAN["port"] = ""
 	}
 	LoggerMap(BAN)
 	if err != nil {
 		LoggerString("ERR")
 	}
+	checkIP(BAN["ip"])
+}
 
+//test
+func checkIP(ipip string) {
+	cip := net.ParseIP(ipip)
+	for _, iprange := range ALLOW {
+		ip, ipnet, err := net.ParseCIDR(iprange)
+		if err != nil {
+			LoggerErr(err)
+		}
+		for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+			if ip.String() == cip.String() {
+				LoggerString("IP FROM ALLOW NETWORK " + ip.String())
+				return
+			}
+		}
+	}
+}
+
+//test
+func inc(ip net.IP) {
+	for j := len(ip)-1; j>=0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
+func NotifyTG(tg_msg string) {
+	LoggerString(tg_msg)
+//	m, err := mtproto.NewMTProto(os.Getenv("HOME") + "/.telegram_go")
+	m, err := mtproto.NewMTProto("/root/.telegram_go")
+	if err != nil {
+		LoggerString("Create failed")
+		LoggerErr(err)
+	}
+	err = m.Connect()
+	if err != nil {
+		LoggerString("Connect failed")
+		LoggerErr(err)
+	}
+	for rcps, each := range TG {
+		rcp := string(rcps)
+		e := string(each)
+		id, err := strconv.ParseInt(each, 10, 32)
+		LoggerString("Send TG_MSG to " + e + " - " + rcp)
+		err = m.SendMsg(int32(id), tg_msg)
+		if (err != nil) {
+			LoggerErr(err)
+		}
+	}
 }
 
 func init() {
@@ -201,12 +261,14 @@ func init() {
 	}
 	ALLOW = conf.Network.Allow
 	DENY = conf.Network.Deny
+	TG = conf.Tg.Rcp
 	stdlog = log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	errlog = log.New(os.Stderr, "", log.Ldate|log.Ltime)
+	NotifyTG("Start/Restart " + _DAEMON_NAME + " " + _DAEMON_DESC)
 }
 
 func main() {
-	srv, err := daemon.New(name, description)
+	srv, err := daemon.New(_DAEMON_NAME, _DAEMON_DESC)
 	if err != nil {
 		errlog.Println("Error: ", err)
 		os.Exit(1)
@@ -263,6 +325,13 @@ func LoggerMapMap(m map[string][]map[string]string) {
 	log.SetOutput(f)
 	log.Print(m)
 	fmt.Println(m)
+}
+
+func LoggerErr(e error) {
+	f, _ := os.OpenFile(LOGPATH, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	log.SetOutput(f)
+	log.Print(e)
+	fmt.Println(e)
 }
 
 func timeFormat() (string) {
