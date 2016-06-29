2cda4f448df0d9a20bbedd33d71772e74b2db4c0
artempr <artyom.prishepa@gmail.com>
2016-06-29 15:28:20 +0600

    .

--- AGIServer.go
+++ AGIServer.go
@@ -675,11 +675,12 @@ func ConfBridgeChannelRedirect(sess *agi.Session) {
 			rc2["Channel"] = bridgepeer.Dat
 			rc2["Exten"] = confno.Dat
 			rc2["Context"] = CONFBRIDGE_CONTEXT
+			ConfBridgeSettings(sess)
 			RedirectChan(rc1)
 			RedirectChan(rc2)
 			LoggerString(fmt.Sprintf("Try create Confbridge CONFNO %s Channel1 %s Channel2 %s",
 				confno.Dat, sess.Env["channel"], bridgepeer.Dat))
-			ConfBridgeSettings(sess)
+
 		}
 	}
 
